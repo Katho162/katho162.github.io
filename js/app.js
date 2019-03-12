6bd6ff1c84f15bc80d65587271f486da38108fa6
Katho162 <patamar101@gmail.com>
2019-03-12 10:02:36 -0300

    Updates

--- js/app.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var thumbnails = document.getElementsByClassName('thumbnail');
-    for (var i = 0; i < thumbnails.length; i++) {
-        var thumbnail = thumbnails[i];
-        thumbnail.style.backgroundImage = "url(" + thumbnail.children[0].src + ")";
-    }
-}, false);
